f9b11ce3173cdd59a106f6580ec2ed2a55b12daf
DESKTOP-JAG7FVD\GrapeCauliflower <daniel.foth@hotmail.com>
2018-02-16 00:24:52 +0000

    Added replay system UI

---
+++ Assets/_Scripts/ReplayUIBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using StaticValuesNamespace;
+
+public class ReplayUIBehaviour : MonoBehaviour {
+
+    public Image replayImg;
+
+    void Start()
+    {
+        if (StaticValues.IsReplay)
+        {
+            StartBlinking();
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+        
+    }
+
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            switch (replayImg.color.a.ToString())
+            {
+                case "0":
+                    replayImg.color = new Color(replayImg.color.r, replayImg.color.g, replayImg.color.b, 1);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+                case "1":
+                    replayImg.color = new Color(replayImg.color.r, replayImg.color.g, replayImg.color.b, 0);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+            }
+        }
+    }
+
+    void StartBlinking()
+    {
+        StopAllCoroutines();
+        StartCoroutine("Blink");
+    }
+
+    void StopBlinking()
+    {
+        StopCoroutine("Blink");
+    }
+}
